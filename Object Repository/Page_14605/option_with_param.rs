c7ff101a4306765fa2383c80c4fa7c849d2bc5ea
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-04-27 19:23:31 +0900

    first commit

---
+++ Object Repository/Page_14605/option_with_param.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>option_with_param</name>
+   <tag></tag>
+   <elementGuidId>9cac4579-69d3-43e1-a77b-f375e8fb0ef7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//option[starts-with(@id,'ui-select-choices-row-') and substring-after(substring-after(@id,'ui-select-choices-row-'),'-')=&quot;${index}&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//option[starts-with(@id,'ui-select-choices-row-') and substring-after(substring-after(@id,'ui-select-choices-row-'),'-')=&quot;${index}&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ README.md
@@ -0,0 +1,11 @@
+Katalon Forum Discussion #14605 - Proposed Solution
+===
+
+## What is this?
+
+This is a simple Katalon Studio project for demonstration purpose.
+You can check this out onto your PC and execute it with your Katalon Studio.
+
+This project requires Katalon Studio version 5.4.1 or higher.
+
+This project is meant to be a proposed solution for the discussion titled ["Not able to select a drop down value using selectOptionByIndex"](https://forum.katalon.com/discussion/comment/14605) in the Katalon Forum.
